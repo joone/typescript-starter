7c684181b4916692349fc30cf3df755eff47d59f
Joone Hur <joone.hur@gmail.com>
2024-04-02 21:07:51 -0700

    Delete unnecessary CLI file and refactor API call logic
    Removed the app-cli.ts file as it was no longer needed. Refactored
    the API call logic in the index.ts file to improve readability and
    maintainability.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,45 @@
+import * as yargs from "yargs";
+
 import app from "./app";
+import * as postAPI from "./api";
 import * as config from "./config.json";
 
 const port = process.env.PORT || config.server.port;
 
-app.listen(port, () => {
-  return console.log(`server is listening on ${port}`);
-});
+const args = yargs
+  .wrap(null)
+  .command("$0 [server]", "typescript-starter", (yargs) => {
+    yargs.positional("server", {
+      description: "Run a web server",
+      type: "string",
+    });
+  })
+  .options({
+    git: {
+      alias: "api",
+      describe:
+        "Execute a REST API call",
+    },
+  })
+  .help()
+  .parseSync();
+
+(() => {
+  if (args.api) {
+    switch (args.api) {
+      case "getPost":
+        console.log(postAPI.getPost());
+        break;
+      default:
+        console.log("Unknown API");
+    }
+  } else {
+    if (args.server) {
+      app.listen(port, () => {
+        return console.log(`server is listening on ${port}`);
+      });
+    }
+  }
+})();
+
+
